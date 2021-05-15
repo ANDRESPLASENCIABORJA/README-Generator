879a59d52d139645a0317718eeea3e5eee6d5e91
APB98 <borja.basolutions@gmail.com>
2021-05-15 13:05:46 -0500

    test

---
+++ README.md
@@ -0,0 +1,46 @@
+# d
+
+  ## Description
+  
+  d.
+  
+  d.
+  
+  d.
+  
+  d.
+  
+  ## Table of contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  d.
+  
+  ## Usage
+  
+  d.
+  
+  ## Contributing
+  
+  d.
+  
+  ## Tests
+  
+  d.
+  
+  ## Licence
+  
+  MIT LICENCE.
+  
+  ## Questions
+  
+  d.
+  
+  d.
+  
+  d.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,73 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+const Choices = require("inquirer/lib/objects/choices");
 
+// TODO: Create a function that returns a license badge based on which license is passed in
+function licenseBadge(license) {
+  if (license === 'MIT LICENCE') {
+    return 'https://img.shields.io/apm/l/vim-mode'
+  }
+  else if (license === 'APACHE LICENCE') {
+    return 'https://img.shields.io/aur/license/android-studio'
+  }
+  else if (license === 'MOZILLA LICENSE') {
+    return '(https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)'
+  }
+};
 
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'NONE') {
+    return '';
+  }
+};
 
 // TODO: Create a function that returns the license link
-
+function licenseLink(license) {
+  if (license === 'MIT LICENCE') {
+    return '(https://opensource.org/licenses/MIT)'
+  }
+  else if (license === 'APACHE LICENCE') {
+    return '(https://opensource.org/licenses/Apache-2.0)'
+  }
+  else if (license === 'MOZILLALICENSE') {
+    return '(https://opensource.org/licenses/MPL-2.0)'
+  }
+};
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'NONE') {
+    return '';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
+function licenseSection(license) {
+  if (license === 'MIT LICENCE') {
+    return '[License: MIT]'
+  }
+  else if (license === 'APACHE LICENCE') {
+    return 'APACHE section'
+  }
+  else if (license === 'MOZILLA LICENSE') {
+    return '[License: MPL 2.0]'
+  }
+};
 
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'NONE') {
+    return '';
+  }
+};
 
+licenseBadge();
+licenseLink();
+licenseSection();
+renderLicenseBadge();
+renderLicenseLink();
+renderLicenseSection();
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return  `# ${data.title}
+  return `# ${data.title}
 
   ## Description
   
@@ -53,7 +104,7 @@ function generateMarkdown(data) {
   
   ## Licence
   
-  ${data.license}
+  ${data.license}.
   
   ## Questions
   
@@ -61,7 +112,7 @@ function generateMarkdown(data) {
   
   ${data.emailQuestions}.
   
-  ${data.phoneQuestions}.`;
-}
+  ${data.phoneQuestions}.`
+};
 
 module.exports = generateMarkdown;
